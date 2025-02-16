5808507bce54076c459f03e18a33bd43e7c31917
xramadhann <xxdhanz@gmail.com>
2025-02-17 06:22:03 +0700

    feat : add parking lot and car asset for testing function logic firebase

--- parkiram/lib/View/HomePageView.dart
+++ parkiram/lib/View/HomePageView.dart
@@ -8,8 +8,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Image(image: AssetImage('assets/logo.png')),
+      body: const Stack(
+        children: <Widget>[
+          Image(
+              image: AssetImage('assets/images/parkinglot.png'),
+              fit: BoxFit.fill),
+          Positioned(
+            top: 510,
+            right: 210,
+            child: Image(
+              image: AssetImage('assets/images/caraset.png'),
+              fit: BoxFit.fill,
+              height: 250,
+            ),
+          ),
+        ],
       ),
     );
   }
