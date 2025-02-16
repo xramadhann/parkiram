84c38689a551c4c249c5d3c07d0364a920b7432b
xramadhann <xxdhanz@gmail.com>
2025-02-17 06:12:08 +0700

    feat : create new conf assets and asset folder

---
+++ parkiram/lib/View/HomePageView.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Image(image: AssetImage('assets/logo.png')),
+      ),
+    );
+  }
+}
